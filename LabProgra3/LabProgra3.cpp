e6579ce310ff81bf568814475f78ef2378a32987
Darksleet <douglaslv96@gmail.com>
2020-05-04 22:48:32 -0600

    Add project files.

---
+++ LabProgra3/LabProgra3.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+
+int main()
+{
+    
+    al_init();
+    al_init_ttf_addon();
+    al_init_font_addon();   
+    ALLEGRO_DISPLAY* display = al_create_display(640, 480);
+    ALLEGRO_FONT* font = al_load_ttf_font("Pacifico.ttf", 64, 0);
+
+    while (true) {
+        al_clear_to_color(al_map_rgb(255, 255, 255));
+        al_draw_text(font, al_map_rgb(0, 0, 0), 0, 0, 0, "Hello World");
+        al_flip_display();
+    }
+
+    al_destroy_display(display);                                
+    al_destroy_font(font);
+
+
+}
+
